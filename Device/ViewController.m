dd6354f0b419eee1241d31a16154065572d234bc
Masaki Hirokawa <info@dolice.net>
2013-05-15 19:56:25 +0900

    Add all files

---
+++ Device/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  Device
+//
+//  Created by Dolice on 2013/03/25.
+//  Copyright (c) 2013 Masaki Hirokawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  //デバイス情報をラベルにセット
+  [self setDeviceInformation];
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+}
+
+- (void)setDeviceInformation
+{
+  //デバイス名称
+  self.labelIOSDevice.text = [Device iOSDevice];
+  //iPhone5であれば1が返る
+  self.labelIphone5.text = [NSString stringWithFormat:@"%@%d", @"isIphone5: ", [Device isIphone5]];
+  //iPhone4/4Sであれば1が返る
+  self.labelIphone4.text = [NSString stringWithFormat:@"%@%d", @"isIphone4: ", [Device isIphone4]];
+  //iPhone3G/3GSであれば1が返る
+  self.labelIphone3.text = [NSString stringWithFormat:@"%@%d", @"isIphone3: ", [Device isIphone3]];
+  //iPad/iPad2/iPad miniであれば1が返る
+  self.labelIpad.text = [NSString stringWithFormat:@"%@%d", @"isIpad: ", [Device isIpad]];
+  //新しい iPad以降であれば1が返る
+  self.labelIpadRetina.text = [NSString stringWithFormat:@"%@%d", @"isIpadRetina: ", [Device isIpadRetina]];
+}
+
+@end
